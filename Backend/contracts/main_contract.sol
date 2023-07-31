acb364f76fefcdd3c7ecfe78c1ce869882858833
Zehaan <zehaan22@iitk.ac.in>
2023-07-31 19:41:24 +0530

    first full commit

---
+++ Backend/contracts/main_contract.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+pragma experimental ABIEncoderV2;
+
+contract Election {
+    struct Voter {
+        bool has_voted; 
+        bool is_reg;
+        uint256 vote; 
+    }
+    struct Candidate {
+        string candidate_name; 
+        uint256 score; 
+    }
+
+    address public leader;
+    mapping(address => Voter) public voters;
+
+    Candidate[] public candidates;
+    constructor(string[] memory candidateNames) {
+        leader = msg.sender;
+        voters[leader].is_reg = true;
+        for (uint64 i = 0; i < candidateNames.length; i++) {
+            candidates.push(Candidate({candidate_name: candidateNames[i], score: 0}));
+        }
+    }
+
+    function registerVoter(address voter) external {
+        require(!voters[voter].has_voted, "The voter already voted.");
+        require(!voters[voter].is_reg, "Voter is already registered.");
+        voters[voter].is_reg=true;
+    }
+
+    function castVote(uint256 pref1, uint256 pref2, uint256 pref3) external {
+        Voter storage voter = voters[msg.sender];
+        require(voter.is_reg, "Voter is not registered.");
+        require(!voter.has_voted, "Already voted.");
+        candidates[pref1].score+=5;
+        candidates[pref2].score+=3;
+        candidates[pref3].score+=1;
+        voter.has_voted=true;
+    }
+
+    function currentStanding() public view returns (Candidate[] memory _candidates) {
+        return candidates;
+    }   
+
+}
--- Frontend/index.js
+++ Frontend/index.js
@@ -1,1 +1,39 @@
+// File to work the frontend reponsiveness of the website
 console.log("Working");
+
+// Placeholder list for voters
+// Would be generated from the contract
+const voters = ['Zehaan22','Aditya22','Ritesh22','Animesh22'];
+
+
+function login(){
+    const userid = document.getElementById('user-id').value;
+    const password = document.getElementById('user-password').value;
+    const firstPref = document.getElementById('user-firstpref').value;
+    const secondPref = document.getElementById('user-secondpref').value;
+    const thirdref = document.getElementById('user-thirdpref').value;
+    var trig = 1;
+    for(var i = 0 ; i<4 ; i++){
+        if (userid === voters[i]){
+            if (password === 'pass123'){ // placeholder for a contract generated password
+                
+                castVote(userid, firstPref, secondPref, thirdref);
+                return;
+            }
+            else{
+                alert("IncorrectPassword !!");
+            }
+        trig = 0;
+        }
+    }
+    if (trig){
+        alert("Invalid ID");
+    }
+    
+    
+}
+
+function castVote(userid, firstPref, secondPref, thirdref){
+    alert("Vote casted: \n" + "Preferrence order: \n" + firstPref + secondPref + thirdref);
+    // Will send this pref to the contract
+}
